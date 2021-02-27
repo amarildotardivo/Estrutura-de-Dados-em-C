e53da88d1686c6c2f251fcdfe2921aff927e8d09
amarildotardivo <58271527+amarildotardivo@users.noreply.github.com>
2021-02-27 17:47:22 -0300

    up

--- Ponteiros/questao-04.c
+++ Ponteiros/questao-04.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int* somatorio(int n, int *v){
     int *w;
 
     w = (int *) malloc(n * sizeof (int));
 
     w[0] = v[0];
-    printf("teste inicial: %d\n", w[0]);
+    printf("Ponteiro W[0]: %d\n\n", w[0]);
 
      for(int i = 0; i < n; i++){
          if(i > 0){
@@ -16,23 +15,29 @@ int* somatorio(int n, int *v){
          }
      }
 
-    for(int i = 0; i < n; i++){
-        printf("%d ", w[i]);
-
-    }
-
-    return 0;
+    return w;
 }
 
 int main (){
-    int  n = 4, v[n];
+    int n = 4;
+    int v[4] = {1, 2, 3, 4};
+    int *w;
 
-    for(int i = 0; i < n; i++){
-        printf("Entre com o %d valor: ", i + 1);
-        scanf("%d", &v[i]);
+    printf("Vetor V:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf(" %d ", v[i]);
     }
+    printf("\n\n");
+    
+    w = somatorio(n, v);
 
-    somatorio(n, v);
+    printf("Ponteiro W:\n");
+    for(int i = 0; i < n; i++){
+        printf(" %d ", w[i]);
+
+    }
+    printf("\n\n");
 
     return 0;
 }
