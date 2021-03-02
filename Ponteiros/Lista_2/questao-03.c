099d85ef827e26c6e1bbe317fd99bc39941b35ce
amarildotardivo <58271527+amarildotardivo@users.noreply.github.com>
2021-03-01 22:57:13 -0300

    Update questao-03.c

--- Ponteiros/Lista_2/questao-03.c
+++ Ponteiros/Lista_2/questao-03.c
@@ -2,31 +2,44 @@
 #include <stdlib.h>
 
 int** ordenar(int n, int* vetorInteiro){
-    int *w, *aux;
+    int *w, *aux, menor, maior = 0;
 
     w = (int *) malloc(n * sizeof (int));
 
-    for(int i = 0; i < n; i++){
-        w[i] = &vetorInteiro[i];
-    }
+    for (int i = 0; i < n; i++){
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n - 1; j++){
+            if(vetorInteiro[i] < menor){
+                menor = vetorInteiro[i];
+
+            }
+            if( vetorInteiro[i] > maior){
+                maior = vetorInteiro[i];
+            }
+    }
+    
+    printf("O menor e: %d\n", menor);
+    printf("O maior e: %d\n", maior);
+    
+   /* for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
             
-            if(w[j] > w[j + 1]){
-                aux = w[j];
-                w[j] = w[j + 1];
-                w[j + 1] = aux;
+            if(vetorInteiro[i] > vetorInteiro[j]){
+                w[j] = &vetorInteiro[i];
+
+            }else if(vetorInteiro[i] < vetorInteiro[j]){
+                w[j] = &vetorInteiro[i];
             }
         }
-    }
+    }*/
 
     return w;
 }
 
 int main(){
     int n = 4;
-    int *vetorInteiro[4] = {4, 3, 2, 1};
+    int *vetorInteiro[4] = {0, 3, 2, 1};
     int **w;
     
     printf("VetorInteiro (valores):\n");
@@ -43,7 +56,7 @@ int main(){
     }
     printf("\n\n");
 
-    w = ordenar(n, vetorInteiro);
+    w = ordenar(n, &vetorInteiro);
 
     printf("Ponteiro W (enderecos):\n");
     for (int i = 0; i < n; i++)
