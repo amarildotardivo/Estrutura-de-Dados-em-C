eee7f98fa4a1ab1c8e5e4eab2fba80ad1e7f8f20
Amarildo Tardivo <amarildotardivo@users.noreply.github.com>
2021-03-26 01:53:06 -0300

    Update mainEsfera.c

--- 3 - TAD/Aula_5/Questao_1/mainEsfera.c
+++ 3 - TAD/Aula_5/Questao_1/mainEsfera.c
@@ -7,7 +7,13 @@ int main(){
     scanf("%f", &diametro);
 
     raio = calculaRaio(diametro);
-    printf("O Raio da Esfera e: %.2f", raio);
+    printf("\n    O Raio da Esfera e: %.2f\n\n", raio);
+    
+    area = calculaArea(raio);
+    printf("    A Area da Esfera e: %.2f\n\n", area);
+    
+    volume = calculaVolume(raio);
+    printf("    O Volume da Esfera e: %.2f\n\n", volume);
 
     return 0;
 }
