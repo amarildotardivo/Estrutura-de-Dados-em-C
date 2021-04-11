9503b86f697453189556c92b84cf5fb38ab5cc9a
Amarildo Tardivo <amarildotardivo@gmail.com>
2021-04-11 14:59:34 -0300

    Update listaEncadeadaFim.h

--- 2 - P2/1 - TAD_LISTAS/Aula 6/Questao_03-ListaEncadeadaFim/listaEncadeadaFim.h
+++ 2 - P2/1 - TAD_LISTAS/Aula 6/Questao_03-ListaEncadeadaFim/listaEncadeadaFim.h
@@ -11,15 +11,20 @@ tipo_lista* criarLista();
 void incluirElemento(tipo_lista* lista, int elemento);
 
 // C) EXCLUSÃO NO FIM DA LISTA
+//Retorna 0 - Quando a Lista está vazia
 int excluirElemento(tipo_lista* lista);
 
 // D) VERIFICAR SE A LISTA ESTÁ VAZIA
+//Retorna 1 - Quando a lista está vazia
+//Retorna 0 - Quando a lista não está vazia
 int verificaListaVazia(tipo_lista* lista);
 
 // E) OBTER A QUANTIDADE DE ELEMENTOS DA LISTA
 int obterQuantidadeElementos(tipo_lista* lista);
  
 // F) APAGAR LISTA
+//Retorna 1 - Quando a lista foi apagada
+//Retorna 0 - Quando a lista está vazia
 int apagarLista(tipo_lista* lista);
 
 // G) OBTER ELEMENTOS DA LISTA
