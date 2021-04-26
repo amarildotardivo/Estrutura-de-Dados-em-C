1bd389b558c85c21ee9b5f3e952ab66767ccc5ff
Amarildo Tardivo <amarildotardivo@gmail.com>
2021-04-26 16:29:12 -0300

    Update questao_06.c

--- 2 - P2/1 - TAD_LISTAS/Aula 9/questao_06.c
+++ 2 - P2/1 - TAD_LISTAS/Aula 9/questao_06.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
-#define tamanho_vetor 1000
-#define range 10000
+#define tamanho_vetor 5
+#define range 5
+
+void insereOrdenado(int vetor[], int valor, int posicao){
+    int aux;
+    
+    if(posicao == 0){
+        vetor[posicao] = valor;
+
+    }else{
+        for(int i = 1; i < posicao; i++){
+            
+            if(vetor[i] > valor){
+                
+                for(int j = posicao; j > i; j--){
+                    vetor[j] = vetor[j-1];
+                }
+                
+                vetor[i] = valor;
+            }
+
+        }
+    }
+}
 
 int verificaIguais(int vetor[], int valor){
 
@@ -27,13 +49,36 @@ void aleatoriosDistintos(int vetor[]){
         while( verificaIguais(vetor, valor) != 1 ){
             valor = rand() % range;
         }
-
-        vetor[i] = valor;
+        printf(" %d ", valor);
+        insereOrdenado(vetor, valor, i);
+        //vetor[i] = valor;
     }
 }
 
+int buscaBinaria(int vetor[], int esq, int dir, int valor){
+    int i;
+    
+    if(dir >= esq){
+        i = esq + (dir - esq) / 2;
+
+        if(vetor[i] == valor){
+            return i;
+        }
+
+        if(vetor[i] > valor){
+            return buscaBinaria(vetor, esq, i - 1, valor);
+
+        }else{
+            return buscaBinaria(vetor, i + 1, dir, valor);
+        }
+    }
+
+    return -1;
+    
+}
+
 int main(){
-    int vetor[tamanho_vetor];
+    int vetor[tamanho_vetor], valor, esq = 0, dir = tamanho_vetor, *comparacoes = 0, resultado;
 
     aleatoriosDistintos(vetor);
 
@@ -44,5 +89,12 @@ int main(){
     }
     printf("\n\n");
     
+    printf("\n  Entre com o valor para busca: ");
+    scanf("%d", &valor);
+
+    resultado = buscaBinaria(vetor, esq, dir, valor);
+
+    printf("\n  Posicao encontrada foi: %d", resultado);
+
     return 0;
 }
