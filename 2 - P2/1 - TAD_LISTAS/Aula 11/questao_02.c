97a9c5268f513c84d17f25f7d676144015b994c0
Amarildo Tardivo <amarildotardivo@gmail.com>
2021-05-05 21:20:15 -0300

    Update questao_02.c

---
+++ 2 - P2/1 - TAD_LISTAS/Aula 11/questao_02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "TAD\tadArvoreBB3.h"
+
+#define tamanho 10
+#define range 200
+
+int main(){
+    
+    srand(time(NULL));
+
+    
+
+    return 0;
+}
