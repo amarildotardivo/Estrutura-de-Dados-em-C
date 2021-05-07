a2cd19b4edcc453429ae92c300f986df33fb6ca6
Amarildo Tardivo <amarildotardivo@gmail.com>
2021-05-07 11:50:51 -0300

    Update questao_02.c

--- 2 - P2/1 - TAD_LISTAS/Aula 11/questao_02.c
+++ 2 - P2/1 - TAD_LISTAS/Aula 11/questao_02.c
@@ -7,9 +7,23 @@
 #define range 200
 
 int main(){
-    
-    srand(time(NULL));
+    tipo_no_arvore arvore;
+    int elemento;
 
+    srand(280);
+
+    arvore = inicializar_arvore(arvore);
+
+    for(int i = 0; i < tamanho; i++){
+        elemento = rand() % range;
+        incluir_no_arvore(arvore, elemento);
+    }
+    //ARVORE GERADA
+    //152 17 7 102 83 48 89 124 137
+    printf("\n\n");
+    printf("\nArvore - Amarildo: 201621200280\n\n");
+    percurso_em_pre_ordem(arvore);
+    printf("\n\n");
     
 
     return 0;
