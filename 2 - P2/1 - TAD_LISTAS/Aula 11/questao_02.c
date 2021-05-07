d4d456abac96b10b85e115995648446f731543ab
Amarildo Tardivo <amarildotardivo@gmail.com>
2021-05-07 17:38:30 -0300

    up aula 11

--- 2 - P2/1 - TAD_LISTAS/Aula 11/questao_02.c
+++ 2 - P2/1 - TAD_LISTAS/Aula 11/questao_02.c
@@ -1,29 +1,51 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#include "TAD\tadArvoreBB3.h"
 
 #define tamanho 10
-#define range 200
+#define range 201
 
 int main(){
-    tipo_no_arvore arvore;
-    int elemento;
+    int elemento[tamanho];
 
     srand(280);
 
-    arvore = inicializar_arvore(arvore);
-
     for(int i = 0; i < tamanho; i++){
-        elemento = rand() % range;
-        incluir_no_arvore(arvore, elemento);
+        elemento[i] = rand() % range + 1;
     }
-    //ARVORE GERADA
-    //152 17 7 102 83 48 89 124 137
+
+    //NUMEROS GERADOS
+    //149 5 55 12 25 87 146 91 142 190
     printf("\n\n");
-    printf("\nArvore - Amarildo: 201621200280\n\n");
-    percurso_em_pre_ordem(arvore);
+    printf("\nAmarildo: 201621200280\n\n");
+    for(int i = 0; i < tamanho; i++){
+        printf(" %d - %p\n\n", elemento[i], &elemento[i]);
+    }
     printf("\n\n");
+
+    /* 
+    
+    149 - 0061FEF0
+
+    5 - 0061FEF4
+
+    55 - 0061FEF8
+
+    12 - 0061FEFC
+
+    87 - 0061FF00
+
+    190 - 0061FF04
+
+    146 - 0061FF08
+
+    91 - 0061FF0C
+
+    142 - 0061FF10
+
+    25 - 0061FF14
+    
+    */
     
 
     return 0;
